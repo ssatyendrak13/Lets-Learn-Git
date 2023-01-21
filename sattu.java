59266f90bc00c49c84cd48f333bbd27e55326db1
Satyendra Kumar <98349239+ssatyendrak13@users.noreply.github.com>
2023-01-21 18:51:41 +0530

    one extra method printString is added in sattu.java

--- sattu.java
+++ sattu.java
@@ -2,4 +2,7 @@ class sattu{
     public static void main(String[] args) {
      System.out.println(3+4);
     }
+    public void printString() {
+        System.out.println("string will be printed ")
+    }
 }
